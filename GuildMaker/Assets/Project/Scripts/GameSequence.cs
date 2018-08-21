e081458f5d0c2435911d30c9fea0d8960427c149
田畑　翔伍 <shogo.tabata@sega.com>
2018-08-21 20:32:31 +0900

    GameSeq Update

--- GuildMaker/Assets/Project/Scripts/GameSequence.cs
+++ GuildMaker/Assets/Project/Scripts/GameSequence.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSequence : MonoBehaviour {
+    public enum State
+    {
+        Init,
+        Routine,
+        Pause,
 
-	// Use this for initialization
-	void Start () {
-		
+
+    }
+    Statemachine<State> _statemachine = new Statemachine<State>();
+    // property
+
+    //時間(月 週 時間)
+    //
+    IEnumerator Init()
+    {
+        yield return null;
+    }
+
+    IEnumerator Routine()
+    {
+        //時間を進める
+        //
+        yield return null;
+    }
+    IEnumerator Pause()
+    {
+        yield return null;
+    }
+	void Awake () {
+		_statemachine.Init(this);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    _statemachine.Update();
 	}
 }
